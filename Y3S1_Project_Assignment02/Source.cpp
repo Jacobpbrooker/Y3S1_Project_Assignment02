7f61a15c2a167572b3e0fe7afd362d3d9627a990
Jacob Brooker <jakepb@hotmail.com>
2022-09-16 10:34:16 -0400

    Add project files.

---
+++ Y3S1_Project_Assignment02/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+typedef struct student_data {
+	std::string first_name;
+	std::string last_name;
+}STUDENT_DATA, *PSTUDENT_DATA;
+
+std::vector<STUDENT_DATA> loadStudents() {
+	std::vector<STUDENT_DATA> student_data;
+
+
+
+
+	return student_data;
+}
+
+int main() {
+
+
+	return 1;
+}
